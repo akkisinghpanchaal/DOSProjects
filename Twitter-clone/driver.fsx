f10f2cb5c7199cea3792c4403710cf9cd90a40f3
Akshay Kumar <akkisinghpanchaal@gmail.com>
2020-11-27 19:09:36 -0500

    fixed issue with driver file

--- Twitter-clone/driver.fsx
+++ Twitter-clone/driver.fsx
@@ -41,7 +41,6 @@ module DriverMod =
         clientActor2 <! FollowUser("akkisingh")
         clientActor <! FollowUser("rajat.rai")
         clientActor <! SendReTweet("@akkisingh ki gaand me danda", 1)
-        clientActor <! 
 
         System.Threading.Thread.Sleep(500)
         serverActor <! ShowData
