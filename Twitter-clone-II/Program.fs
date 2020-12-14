a6bb2a399d94a9b3a6d04a021d6b80ac06167ffc
Akshay Kumar <akkisinghpanchaal@gmail.com>
2020-12-13 20:19:18 -0500

    test websocket code | testing not working

--- Twitter-clone-II/Program.fs
+++ Twitter-clone-II/Program.fs
@@ -4,9 +4,21 @@ open System
 
 
 open Suave
-open Suave.Filters
+open Suave.Http
 open Suave.Operators
+open Suave.Filters
 open Suave.Successful
+open Suave.Files
+open Suave.RequestErrors
+open Suave.Logging
+open Suave.Utils
+
+open System
+open System.Net
+
+open Suave.Sockets
+open Suave.Sockets.Control
+open Suave.WebSocket
 open Newtonsoft.Json
 open Newtonsoft.Json.Serialization
 
@@ -19,6 +31,47 @@ type Credentials = {
     Uid: string
     Password: string
 }
+let ws (webSocket : WebSocket) (context: HttpContext) =
+  socket {
+    // if `loop` is set to false, the server will stop receiving messages
+    let mutable loop = true
+
+    while loop do
+      // the server will wait for a message to be received without blocking the thread
+      let! msg = webSocket.read()
+
+      match msg with
+      // the message has type (Opcode * byte [] * bool)
+      //
+      // Opcode type:
+      //   type Opcode = Continuation | Text | Binary | Reserved | Close | Ping | Pong
+      //
+      // byte [] contains the actual message
+      //
+      // the last element is the FIN byte, explained later
+      | (Text, data, true) ->
+        // the message can be converted to a string
+        let str = UTF8.toString data
+        let response = sprintf "response to %s" str
+
+        // the response needs to be converted to a ByteSegment
+        let byteResponse =
+          response
+          |> System.Text.Encoding.ASCII.GetBytes
+          |> ByteSegment
+
+        // the `send` function sends a message back to the client
+        do! webSocket.send Text byteResponse true
+
+      | (Close, _, _) ->
+        let emptyResponse = [||] |> ByteSegment
+        do! webSocket.send Close emptyResponse true
+
+        // after sending a Close message, stop the loop
+        loop <- false
+
+      | _ -> ()
+    }
 
 let JSON v =
     let jsonSerializerSettings = JsonSerializerSettings()
@@ -47,7 +100,8 @@ let parseCreds (req : HttpRequest) =
 let loginUser (req: HttpRequest) = 
     let creds = parseCreds req
     if creds.Password = "lassan" then
-        OK "You're good!"
+        // OK "You're good!"
+        handShake ws
     else
         OK "maa chuda!"
 
